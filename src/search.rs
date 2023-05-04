1c98eef2293059d0285b14c43939db7115f315c1
j6k1 <nanasi18@gmail.com>
2023-05-04 16:50:31 +0900

    Changed so that send_info is always called during Root node search.

--- src/search.rs
+++ src/search.rs
@@ -565,8 +565,6 @@ impl<L,S> Root<L,S> where L: Logger + Send + 'static, S: InfoSender {
 
         let mut is_timeout = false;
 
-        let mut best_score = Score::NEGINFINITE;
-
         loop {
             if threads == 0 {
                 let r = self.receiver.recv();
@@ -579,13 +577,9 @@ impl<L,S> Root<L,S> where L: Logger + Send + 'static, S: InfoSender {
 
                 match r {
                     RootEvaluationResult::Value(n, win, nodes,  mvs) => {
-                        if n.nodes > 0 && best_score <= n.computed_score() {
-                            best_score = n.computed_score();
+                        let pv = mvs.clone();
 
-                            let pv = mvs.clone();
-
-                            self.send_info(env, &pv, n.computed_score())?;
-                        }
+                        self.send_info(env, &pv, n.computed_score())?;
 
                         node.win = node.win + -win;
                         node.nodes += nodes;
